793e6427a71ffa6983da59207a8bd0bd1bcf59df
pamelaisrael <162122679+pamelaisrael@users.noreply.github.com>
2024-11-04 08:15:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+criaCartao(
+    'História',
+    'Qual foi a idepêdencia do Brasil?',
+    '7 de setembro de 1622'
+)
+criaCartao(
+    'Matemática',
+    'Qual é o valor de x na equação 3x+5=20?',
+    'O valor de x é 5'
+)
+criaCartao(
+    'Inglês',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi.'
+)
+criaCartao(
+    'Inglês',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hello.'
+)
